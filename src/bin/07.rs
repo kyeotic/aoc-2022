d572cb0649ca8ce95ef3bffe225255a74ead9759
Tim Kye <tkye@squareup.com>
2023-10-29 12:45:28 -0700

    worked with rc

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,41 +1,49 @@
 #![allow(dead_code, unused_imports)]
 use aoc::{get_input, report};
 use itertools::Itertools;
-use std::{collections::HashMap, env::join_paths};
+use std::{cell::RefCell, collections::HashMap, env::join_paths, rc::Rc};
 
 // Since this puzzle could use a state machine, and I'm doing this to learn rust not learn algos
 // I again chose to build this like a real program instead of code-golfing it
 // It was difficult, but it forced me to contend with borrow-checker and lifetimes for the first time
 
 #[derive(Debug)]
-struct Dir<'a> {
+struct Dir {
     name: String,
     path: String,
-    files: HashMap<String, File>,
-    dirs: HashMap<String, &'a Dir<'a>>,
+    files: RefCell<HashMap<String, File>>,
+    dirs: RefCell<HashMap<String, Rc<Dir>>>,
 }
 
-impl<'a> Dir<'a> {
-    fn new(name: String, parent_path: &str) -> Self {
+impl Dir {
+    fn new(name: String, parent_path: Option<&str>) -> Self {
         Self {
-            path: [parent_path, &name.clone()].join("/").to_owned(),
-            name,
-            files: HashMap::new(),
-            dirs: HashMap::new(),
+            name: name.clone(),
+            path: match parent_path {
+                Some(p) => [p, &name].join("/").to_owned(),
+                _ => name,
+            },
+            files: RefCell::new(HashMap::new()),
+            dirs: RefCell::new(HashMap::new()),
         }
     }
 
     fn get_size(&self) -> u32 {
-        self.files.values().map(|f| f.size).sum::<u32>()
-            + self.dirs.values().map(|d| d.get_size()).sum::<u32>()
+        self.files.borrow().values().map(|f| f.size).sum::<u32>()
+            + self
+                .dirs
+                .borrow()
+                .values()
+                .map(|d| d.get_size())
+                .sum::<u32>()
     }
 
-    fn add_dir(&'a mut self, dir: &'a Dir<'a>) {
-        self.dirs.insert(dir.name.to_owned(), dir);
+    fn add_dir(&self, dir: Rc<Dir>) {
+        self.dirs.borrow_mut().insert(dir.name.to_owned(), dir);
     }
 
-    fn add_file(&mut self, file: File) {
-        self.files.insert(file.name.to_owned(), file);
+    fn add_file(&self, file: File) {
+        self.files.borrow_mut().insert(file.name.to_owned(), file);
     }
 }
 
@@ -61,7 +69,7 @@ impl LineRead {
             Self::MoveUp
         } else if line.starts_with("$ cd ") {
             Self::MoveInto(line.replace("$ cd ", ""))
-        } else if line.is_empty() || line.starts_with("dir ") || line.starts_with("ls") {
+        } else if line.is_empty() || line.starts_with("dir ") || line.starts_with("$ ls") {
             Self::NoOp
         } else if line.len() > 1 && line.chars().nth(0).unwrap().is_numeric() {
             let (size, name) = line.split(" ").collect_tuple().unwrap();
@@ -70,42 +78,48 @@ impl LineRead {
                 size: size.parse::<u32>().unwrap(),
             })
         } else {
+            println!("lr parse {}", line);
             unreachable!("no lineread type")
         }
     }
 }
 
 #[derive(Debug)]
-struct FileSystem<'a> {
-    dirs: HashMap<String, Dir<'a>>,
+struct FileSystem {
+    dirs: RefCell<HashMap<String, Rc<Dir>>>,
 }
 
 // Ownership for the directories must live somewhere accessible by the line-reader
 // If it lived in main() using a Vec we would lose dirs when moving to root
 // If it lived in the chain of dirs it would require walking the tree to get mut refs
 // So it lives in this struct, which keeps them in an easily-accessible hash-map
-impl<'a> FileSystem<'a> {
+impl FileSystem {
     fn new() -> Self {
-        let root = Dir::new("/".to_owned(), "");
-        let mut dirs = HashMap::new();
-        dirs.insert(root.path.to_owned(), root);
+        let root = Rc::new(Dir::new("/".to_owned(), None));
+        let dirs = RefCell::new(HashMap::new());
+        dirs.borrow_mut().insert(root.path.to_owned(), root);
         Self { dirs }
     }
 
-    fn get_root(&self) -> &'a Dir {
-        self.dirs.get("/").unwrap()
+    fn get_root(&self) -> Rc<Dir> {
+        self.dirs.borrow().get("/").unwrap().clone()
     }
 
-    fn get_dir(&mut self, path: &str) -> &mut Dir<'a> {
-        self.dirs.get_mut(path).unwrap()
+    fn get_dir(&self, path: &str) -> Rc<Dir> {
+        self.dirs.borrow_mut().get(path).unwrap().clone()
     }
 
-    fn create_dir(&mut self, parent: &'a mut Dir<'a>, dir_name: String) -> &'a Dir {
-        let dir: Dir<'a> = Dir::new(dir_name.to_owned(), &parent.path);
-        self.dirs.insert(dir.path.to_owned(), dir);
-        let dir_out = self.dirs.get(dir_name.as_str()).unwrap();
-        parent.add_dir(&dir_out);
-        &dir_out
+    fn create_dir(&self, parent: Rc<Dir>, dir_name: String) -> Rc<Dir> {
+        let dir = Rc::new(Dir::new(dir_name.to_owned(), Some(&parent.path)));
+        self.dirs
+            .borrow_mut()
+            .insert(dir.path.to_owned(), dir.clone());
+        parent.add_dir(dir.clone());
+        dir.clone()
+    }
+
+    fn dirs_vec(&self) -> Vec<Rc<Dir>> {
+        self.dirs.borrow().values().map(|d| d.clone()).collect()
     }
 
     // fn create_file(&mut self, )
@@ -119,7 +133,7 @@ fn main() {
     //      match
     //          move_up =>
 
-    let mut fs = FileSystem::new();
+    let fs = FileSystem::new();
     let mut dir_stack = vec![fs.get_root().path.clone()];
 
     for line in input.lines() {
@@ -147,10 +161,35 @@ fn main() {
         }
     }
 
-    println!("{:?}", fs);
+    // println!(
+    //     "{:?}",
+    //     fs.dirs_vec()
+    //         .iter()
+    //         .map(|d| (d.name.to_owned(), d.get_size()))
+    //         .collect::<Vec<(String, u32)>>()
+    // );
 
-    let a = 1;
-    let b = 2;
+    let total_size = 70000000;
+    let total_used = fs.get_root().get_size();
+    let unused = total_size - total_used;
+    let update_size = 30000000;
+    let needed_size = update_size - unused;
+
+    let a: u32 = fs
+        .dirs_vec()
+        .iter()
+        .map(|d| d.get_size().clone())
+        .filter(|d| d <= &(100_000 as u32))
+        .sum();
+    let b = fs
+        .dirs_vec()
+        .iter()
+        .map(|d| d.get_size().clone())
+        .filter(|d| d >= &needed_size)
+        .sorted()
+        .rev()
+        .last()
+        .unwrap();
 
     report(&a, &b);
 
